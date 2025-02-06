cff3627a7bc23a43853d0d0dc9ddf5d0be033fcf
meera <meera.malhot@gmail.com>
2025-02-06 11:53:40 -0500

    redoing paths

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [ 
     # path(r'', views.home, name="home"),
-    path(r'quote', views.quote, name="quote"),
-    path(r'quote', views.quote, name="quote"),
+    path(r'', views.quote, name="quote"),
     path(r'show_all', views.show_all, name="show_all"),
     path(r'about', views.about, name="about"),
 ]
