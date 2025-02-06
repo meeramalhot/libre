1b2d060a2c5872f3d156a2bc48384dac0a689a3a
meera <meera.malhot@gmail.com>
2025-02-06 16:15:37 -0500

    urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [ 
     # path(r'', views.home, name="home"),
-    path(r'', views.quote, name="quote"),
-    path(r'show_all', views.show_all, name="show_all"),
-    path(r'about', views.about, name="about"),
+    path(r'', views.main, name="main"),
+    path(r'order', views.order, name="order"),
+    path(r'confirmation', views.about, name="confirmation"),
 ]
