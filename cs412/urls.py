139c11018e00cf286b575e818420e20c6fa3efd6
meera <meera.malhot@gmail.com>
2025-02-06 16:12:46 -0500

    restaurant starter

--- cs412/urls.py
+++ cs412/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("hw/", include("hw.urls")),
     path("quotes/", include("quotes.urls")),
+    path("restaurant/", include("restaurant.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
