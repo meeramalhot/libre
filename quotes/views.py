cce2331526c49b97304d68e969d174b0ce538839
meera <meera.malhot@gmail.com>
2025-02-06 11:28:02 -0500

    django loop

---
+++ quotes/views.py
@@ -0,0 +1,51 @@
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+import random
+
+quotes_list =["My library is an archive of longings",
+         "Attention is vitality. It connects you with others. It makes you eager. Stay eager", 
+         "To paraphrase several sages: Nobody can think and hit someone at the same time",
+         ]
+
+images = ['/static/image1.jpg', '/static/image2.webp', '/static/image3.webp']
+
+
+def quote(request):
+  template_name = 'quotes/quote.html'
+  rand_index = random.randint(0,2)
+  display_quote = quotes_list[rand_index]
+  diplay_image= images[rand_index]
+
+  context = {
+        'quote': display_quote,
+        'image' : diplay_image,
+    }
+
+  return render(request, template_name, context)
+
+def show_all(request):
+  template_name = 'quotes/show_all.html'
+
+  context = {
+    'images': images,
+    'quotes_list': quotes_list,
+  }
+
+  print(context)
+  return render(request, template_name, context)
+
+def base(request):
+  template_name = 'quotes/base.html'
+
+  return render(request, template_name)
+
+def about(request):
+  template_name = 'quotes/about.html'
+  context = {
+    'welcome': "Welcome",
+    'string_one': "This application displays the quotes of the author Susan Sontag.",
+    'string_two': "Susan Lee Sontag was a writer, essayist, critic, and public intellectual. She is known for her essays like On Photagraphy and Notes On Camp."
+  }
+  return render(request, template_name, context)
