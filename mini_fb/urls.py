535be26d61c459ad43215cdb8710c5600adc8444
meera <meera.malhot@gmail.com>
2025-02-19 17:43:25 -0500

    show all table

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,3 +1,6 @@
-urlpatterns = [ 
+from django.urls import path
+from mini_fb.views import ShowAllProfilesView
 
+urlpatterns = [
+  path('show_all', ShowAllProfilesView.as_view(), name="show_all"),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-from .models import Person
-from django.views.generic import ListView
+from .models import Profile
+from django.views.generic import ListView, DetailView
+
+class ShowAllProfilesView(ListView):
+    '''Create a subclass of ListView to display all blog profiles.'''
+
+    model = Profile # retrieve objects of type Article from the database
+    template_name = 'mini_fb/show_all.html'
+    context_object_name = 'profiles' # how to find the data in the template file
