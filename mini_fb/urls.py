c1551bcd166dd239c4c058dd7fc9855086f8fcbc
meera <meera.malhot@gmail.com>
2025-02-19 18:28:14 -0500

    .

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from mini_fb.views import ShowAllProfilesView
 
 urlpatterns = [
-  path('show_all', ShowAllProfilesView.as_view(), name="show_all"),
+  path('', ShowAllProfilesView.as_view(), name="show_all_profiles"),
+  path('show_all_profiles', ShowAllProfilesView.as_view(), name="show_all_profiles"),
 ]
