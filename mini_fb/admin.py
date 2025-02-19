ed257de6c13e61da01883779a7353b2cd30adac3
meera <meera.malhot@gmail.com>
2025-02-19 14:07:53 -0500

    database changes

--- mini_fb/admin.py
+++ mini_fb/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Person
-admin.site.register(Person)
+from .models import Profile
+admin.site.register(Profile)
--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
-class Person(models.Model):
-    '''Encapsulate the idea of an Article by some author.'''
+class Profile(models.Model):
+    '''Encapsulate profile info for a person.'''
 
     # data attributes of a Article:
-    name = models.TextField(blank=False)
-    au = models.TextField(blank=False)
-    text = models.TextField(blank=False)
-    published = models.DateTimeField(auto_now=True)
+    first_name = models.TextField(blank=False)
+    last_name = models.TextField(blank=False)
+    city = models.TextField(blank=False)
+    email = models.TextField(blank=False)
+    image_url = models.URLField(blank=True) ## new
+
     
     def __str__(self):
         '''Return a string representation of this Article object.'''
-        return f'{self.name} by {self.au}'
+        return f'{self.first_name} {self.last_name}'
