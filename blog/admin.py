53370c2a9f6eb0786c8df81a6497a562f11055ad
meera <meera.malhot@gmail.com>
2025-02-24 20:45:47 -0500

    database

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
+# blog/admin.py
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
+from .models import Article, Comment
 admin.site.register(Article)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -19,4 +19,17 @@ class Article(models.Model):
     def get_absolute_url(self):
         '''Return the URL to display one instance of this model.'''
         return reverse('article', kwargs={'pk':self.pk})
+
+
+class Comment(models.Model):
+    '''Encapsulate the idea of a Comment on an Article.'''
     
+    # data attributes of a Comment:
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
+    author = models.TextField(blank=False)
+    text = models.TextField(blank=False)
+    published = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        '''Return a string representation of this Comment object.'''
+        return f'{self.text}'
