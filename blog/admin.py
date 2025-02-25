4c6e22da65ce20d50542558daa2f1ad2c60ffbe1
meera <meera.malhot@gmail.com>
2025-02-25 09:44:40 -0500

    Revert "database"
    This reverts commit 53370c2a9f6eb0786c8df81a6497a562f11055ad.
    
    reset to restart after db was added

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,5 @@
-# blog/admin.py
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article, Comment
+from .models import Article
 admin.site.register(Article)
-admin.site.register(Comment)
