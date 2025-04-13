ad7ba6a4b559012ee5d050dff668203058248a31
meera <meera.malhot@gmail.com>
2025-04-13 14:05:56 -0400

    registering models

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Profile, Review, Book, Friend
+admin.site.register(Profile)
+admin.site.register(Review)
+admin.site.register(Book)
+admin.site.register(Friend)
