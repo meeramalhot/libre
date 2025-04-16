175e8158b074472bd9067c8907f0b908004f5359
meera <meera.malhot@gmail.com>
2025-04-15 23:07:25 -0400

    forms start

--- project/views.py
+++ project/views.py
@@ -32,15 +32,17 @@ class ShowAllProfilesView(ListView):
 
         return super().dispatch(request, *args, **kwargs)
 
-
-
     def get_context_data(self, **kwargs):
-        '''Return the dictionary of context variables for use in the template.'''
         context = super().get_context_data(**kwargs)
-
-        profile = UserProfile.objects.get(user=self.request.user)
-        context['profile'] = profile
+        if self.request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=self.request.user)
+            except UserProfile.DoesNotExist:
+                profile = None
+            
+            context['profile'] = profile
         return context
+
 
 class ProfileDetailView(DetailView):
     model = UserProfile
@@ -79,10 +81,36 @@ class CreateProfileView(CreateView):
                 login(self.request, new_user)
                 form.instance.user = new_user
                 form.instance.date_joined = timezone.now()
+                
                 return super().form_valid(form)
         else:
-            print("invalid form!")
-            return self.form_invalid(form)
+                    print("invalid form!")
+                    return self.form_invalid(form)
         
     def get_login_url(self) -> str:
         return reverse('login')
+
+class CreateReviewView(CreateView):
+        '''A view to handle creation of a new Comment on an Article.'''
+
+        template_name = "project/create_status_form.html"
+
+        def get_success_url(self):
+            '''Provide a URL to redirect to after creating a new status message.'''
+            profile = UserProfile.objects.get(user=self.request.user)
+            return reverse('show_profile', kwargs={'pk': profile.pk})
+        
+        def get_context_data(self, **kwargs):
+            '''Return the dictionary of context variables for use in the template.'''
+            context = super().get_context_data(**kwargs)
+
+            profile = UserProfile.objects.get(user=self.request.user)
+            context['profile'] = profile
+            return context
+        
+        def get_login_url(self) -> str:
+            '''return the URL required for login'''
+            return reverse('login')
+        
+        def get_object(self):
+            return UserProfile.objects.get(user=self.request.user)
